0a286e473abbf2a4e77e8b69c20589e51d54edbc
Steven Hicks <shicks1000@gmail.com>
2017-10-31 19:44:38 +0100

    fixed typo and replaced object assign

--- src/createOptimisticMiddleware.js
+++ src/createOptimisticMiddleware.js
@@ -1,18 +1,16 @@
 import { BEGIN, COMMIT, REVERT } from "redux-optimistic-ui";
 
-let defaultOptions = {
-    success: '_SUCESS',
-    error: '_ERROR',
+const defaultOptions = {
+    _SUCCESS: '_SUCCESS',
+    _ERROR: '_ERROR',
 }
 
 export const createOptimisticMiddleware = options = defaultOptions => {
     
     options = { ...defaultOptions, ...options};
     
-    
     //All redux action types that are optimistic have the following suffixes
-    const _SUCCESS = options._SUCCESS;
-    const _ERROR = options._ERROR;
+    const { _SUCCESS, _ERROR } = options;
 
     //Each optimistic item will need a transaction Id to internally match the BEGIN to the COMMIT/REVERT
     let nextTransactionID = 0;
@@ -22,10 +20,13 @@ export const createOptimisticMiddleware = options = defaultOptions => {
         const { type, meta, error, payload } = action;
     
         // Ignore actions without isOptimistic flag
-        if (!meta || !meta.isOptimistic) return next(action);
+        if (!meta || !meta.isOptimistic) {
+            return next(action);
+        } 
     
-        const isSuccessAction = type.endsWith(_SUCCESS);
-        const isErrorAction = type.endsWith(_ERROR);
+        const isSuccessAction = type.endsWith(_SUCCESS),
+              isErrorAction = type.endsWith(_ERROR);
+
         //Response from server, handled in epic-middleware
         if (isSuccessAction || isErrorAction) {
             return next(action);
@@ -34,11 +35,16 @@ export const createOptimisticMiddleware = options = defaultOptions => {
         // Now that we know we're optimistically updating the item, give it an ID
         let transactionID = nextTransactionID++;
         // Sending to server; extend the action.meta to let it know we're beginning an optimistic update
-        return next(
-            Object.assign({}, action, {
-                meta: { optimistic: { type: BEGIN, id: transactionID } }
-            })
-        );
+        
+        return next({
+            ...action,
+            meta: {
+                optimistic: {
+                    type: BEGIN,
+                    id: transactionID
+                }
+            }
+        });
     };
 
     return optimisticMiddleware;  
